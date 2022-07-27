7010608db9853165264c939902c46de44e9d6f0d
librairica <erica.handelman@gmail.com>
2022-07-26 20:47:40 -0400

    return dummy data in getContacts resolver

--- resolvers.js
+++ resolvers.js
@@ -10,7 +10,23 @@ const resolvers = {
       },
       getContacts: async (parent, args) => {
         try {
-          return await Contact.find();
+          return [
+            {
+              "id": "6beda137-cff7-4c85-b062-3902913ecf5c",
+              "firstName":"Erica",
+              "lastName": "Handelman",
+              "birthday": "1988-09-30",
+              "birthTimestamp": "",
+              "sunSignEmoji": "♎︎"
+            },
+            {
+              "id": "e2a78342-1a77-44b7-a6f8-1e0c140cb69b",
+              "firstName":"Leanne",
+              "lastName": "Blackwell",
+              "birthday": "1988-07-26",
+              "sunSignEmoji": "♌︎"
+            }
+          ]
         } catch (error) {
           throw new Error(error);
         }
