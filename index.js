ffac6e2e3048b56e1912be765bbcb75a27d5a3c0
librairica <handelman.el@pg.com>
2023-09-23 19:13:02 -0400

    get contacts working with hardcoded data

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 const { ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose');
-require("dotenv").config();
-
-// The config file contains any project configuration
-// MONGODB will be something like this "'mongodb://username:password@host:port/database?options...'", you can get your own from Mongodb.com.
-// PORT: process.env.PORT || '5000', if no env variables.
-
-// const { MONGODB ,PORT } = require('./config');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
+// const resolvers = require('./graphql/resolvers');
+const contacts = [
+  {
+    id: 123,
+    firstName: "Leanne",
+    lastName: "Blackwell",
+    birthday: "07-26-88",
+  },
+  {
+    id: 223,
+    firstName: "Shelby",
+    lastName: "Steinmeyer",
+    birthday: "03-11-87",
+  },
+];
+const resolvers = {
+  Query: {
+    getContacts: () => contacts,
+  },
+};
 
+async function run() {
+  // MONGO DB CONNECT
+  require('dotenv').config()
+  const username = encodeURIComponent(process.env.DB_USER);
+  const password = encodeURIComponent(process.env.PASSWORD);
+  const cluster = process.env.CLUSTER;
+
+  let uri = `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority"`;
+  console.log("uri: " + uri)
+  // MONGO DB
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+
+  // APOLLO SERVER
+  // The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
+await server.listen();
 
-mongoose
-  .connect(process.env.MONGODB, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
-  .then(() => {
-    console.log('MongoDB is connected ...');
-    return server.listen({
-      port: process.env.PORT
-    });
-  })
-  .then(res => {
-    console.log('Server running at ', res.url);
-  });
+console.log(`🚀  Server ready at: http://localhost:4000/graphql`);
+}
+run().catch(console.dir);
+
